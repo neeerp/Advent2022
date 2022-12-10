b196fb99b9d007c317bf61750bfd951a3a92ae51
David Goldstein <goldvi@amazon.com>
2022-12-10 15:54:10 -0500

    Complete D1P2

--- src/day/day1.ts
+++ src/day/day1.ts
@@ -8,11 +8,22 @@ class Day1 extends Day {
   solvePartOne(input: string): string {
     const elves = input.split('\n\n');
 
-    const max_cals =  elves.reduce((max, elf) => {
+    const max_cals = elves.reduce((max, elf) => {
       return Math.max(max, this.calsForElf(elf));
     }, 0);
 
     return max_cals.toString();
+  }
+
+  solvePartTwo(input: string): string {
+    const elves = input.split('\n\n');
+
+    const max_cals = elves.reduce((max, elf) => {
+      return this.maintainTopThree(max, this.calsForElf(elf));
+    }, [0, 0, 0]);
+    const total = max_cals[0] + max_cals[1] + max_cals[2];
+
+    return total.toString();
   }
 
   calsForElf(elf: string): number {
@@ -29,8 +40,13 @@ class Day1 extends Day {
     return parseInt(int, radix);
   }
 
-  solvePartTwo(input: string): string {
-    return input;
+  maintainTopThree(topThree: number[], candidate: number) {
+    topThree.sort((a, b) => a - b);
+    if (topThree[0] < candidate) {
+      topThree[0] = candidate;
+    }
+
+    return topThree;
   }
 }
 
--- src/day/spec/day1.spec.ts
+++ src/day/spec/day1.spec.ts
@@ -5,4 +5,9 @@ describe('On Day 1', () => {
     const input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
     expect(day1.solvePartOne(input)).toBe('24000');
   })
+
+  it('Part 2 returns the three fattest elves', () => {
+    const input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
+    expect(day1.solvePartTwo(input)).toBe('45000');
+  })
 });
